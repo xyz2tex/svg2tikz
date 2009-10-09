cd7d24c1ca66dee3cfb6f26673a895e54888cb26
Kjell Magne Fauske <kjellmf@gmail.com>
2009-10-09 09:47:05 +0200

    Updated tests

--- tests/test_svg2tikz.py
+++ tests/test_svg2tikz.py
@@ -23,10 +23,37 @@ class InterfaceTest(unittest.TestCase):
     
     def test_basicsvg(self):
         code = convert_file(basic_svg)
-        self.failUnless('rect' in code)
+        assert 'rect' in code
+    
     def test_basic_codeonly(self):
-        code = convert_file(svg_file=basic_svg, codeoutput="codeonly")
-        self.failIf('documentclass' in code)
+        code = convert_file(basic_svg, codeoutput="codeonly")
+        assert 'documentclass' not in code
+        assert r'\begin{tikzpicture}' not in code
+
+    def test_basic_figonly(self):
+        code = convert_file(basic_svg, codeoutput="figonly")
+        assert 'documentclass' not in code
+        assert r'\begin{tikzpicture}' in code
+
+
+paint_svg = r"""<?xml version="1.0" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
+  "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg width="12cm" height="4cm" viewBox="0 0 1200 400"
+     xmlns="http://www.w3.org/2000/svg" version="1.1"
+     fill="red">
+  <desc>Example rect01 - rectangle with sharp corners</desc>
+  <!-- Show outline of canvas using 'rect' element -->
+  <rect x="1" y="1" width="1198" height="398"
+     stroke="blue" stroke-width="2"/>
+</svg>
+"""
+
+class PaintingTest(unittest.TestCase):
+    
+    def test_inherited_fill(self):
+        code = convert_file(paint_svg, codeoutput="codeonly")
+        assert 'fill=red' in code
 
 
 if __name__ == '__main__':
