9d8df7dbb6b8b8691d5e74441566cc048c22756c
kjellmf <kjellmf@gmail.com>
2014-04-17 10:59:17 +0200

    Update to new project url

--- setup.py
+++ setup.py
@@ -2,30 +2,29 @@ from distutils.core import setup
 #from setuptools import setup
 
 setup(name='svg2tikz',
-    version='0.2',
-    description='An SVG to TikZ converter',
-    author='Kjell Magne Fauske',
-    author_email='kjellmf@gmail.com',
-    url="http://code.google.com/p/inkscape2tikz/",
-    download_url="http://code.google.com/p/inkscape2tikz/downloads/",
-    packages=['svg2tikz', 'svg2tikz.extensions', 'svg2tikz.inkexlib'],
+      version='0.2',
+      description='An SVG to TikZ converter',
+      author='Kjell Magne Fauske',
+      author_email='kjellmf@gmail.com',
+      url="https://github.com/kjellmf/svg2tikz",
+      packages=['svg2tikz', 'svg2tikz.extensions', 'svg2tikz.inkexlib'],
 
-    scripts=['scripts/svg2tikz'],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent'
-        'Programming Language :: Python',
-        'Topic :: Scientific/Engineering :: Visualization',
-        'Topic :: Text Processing :: Markup :: LaTeX',
-        'Topic :: Utilities',
-        ],
-    entry_points={
-        'console_scripts': [
-            'svg2tikz = svg2tikz.extensions.tikz_export:main_cmdline',
-            ]
-    }
+      scripts=['scripts/svg2tikz'],
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent'
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Visualization',
+          'Topic :: Text Processing :: Markup :: LaTeX',
+          'Topic :: Utilities',
+      ],
+      entry_points={
+          'console_scripts': [
+              'svg2tikz = svg2tikz.extensions.tikz_export:main_cmdline',
+          ]
+      }
 )
