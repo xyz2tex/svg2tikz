5b18e816a52e73c4a7df3d170d0117ee77a95e6e
Aaron Fenyes <afenyes@math.toronto.edu>
2020-08-05 19:38:25 +0200

    Convert translation to user units

--- svg2tikz/extensions/tikz_export.py
+++ svg2tikz/extensions/tikz_export.py
@@ -961,7 +961,7 @@ class TikZPathExporter(inkex.Effect):
         options = []
         for cmd, params in transform:
             if cmd == 'translate':
-                x, y = params
+                x, y = [self.unittouu(str(val)) for val in params]
                 options.append("shift={(%s,%s)}" % (round(x, 5) or '0', round(y, 5) or '0'))
                 # There is bug somewere.
                 # shift=(400,0) is not equal to xshift=400
